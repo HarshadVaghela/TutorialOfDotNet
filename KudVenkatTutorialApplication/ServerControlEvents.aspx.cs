749bb35ad641cd777f5f5d75c76c615271804977
Harshad Vaghela <harshadvaghela5@outlook.com>
2018-11-24 12:35:48 +0530

    Import Project

---
+++ KudVenkatTutorialApplication/ServerControlEvents.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KudVenkatTutorialApplication
+{
+    public partial class ServerControlEvents : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("PostBack Event" + "<br/>");
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Response.Write("Textbox Changed Event" + "<br/>");
+        }
+    }
+}
---
+++ KudVenkatTutorialApplication/SessionExamplePage1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KudVenkatTutorialApplication
+{
+    public partial class SessionExamplePage1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["ClicksCount"] == null)
+                {
+                    Session["ClicksCount"] = 0;
+                }
+                TextBox1.Text = Session["ClicksCount"].ToString();
+            }
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+           int ClicksCount = Convert.ToInt32(Session["ClickCount"]) + 1;
+            TextBox1.Text = ClicksCount.ToString();
+            Session["ClickCount"] = ClicksCount;
+        }
+
+
+    }
+}
